2a89d41511a3348ee2f42aebea84425ed655d3ab
joaovianayt <joaoparaty87@gmail.com>
2022-03-26 16:30:37 -0300

    Initial commit

---
+++ Exercícios/Ex 009/jupyter.py
@@ -0,0 +1,9 @@
+from ctypes import pythonapi
+from distutils.command.install_lib import PYTHON_SOURCE_EXTENSION
+
+
+PYTHON_SOURCE_EXTENSION
+pythonapi
+python (print)
+123
+print("qualquer coisa")
---
+++ README.md
@@ -0,0 +1,2 @@
+# html-css
+ Curso de HTML5 E CSS3
